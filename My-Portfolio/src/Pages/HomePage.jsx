a8e5f15e49cfbe8568dace5a8289263f3c6d3ccc
rahul22333'git config --global user.email rahul7488071@gmail.comgit config --global user.name rahul22333git config --listgit config --global user.name "rahul22333"git config --global user.name "rahul22333 <rahul7488071@gmail.com>
2025-07-31 17:32:55 +0530

    homepage-added

--- My-Portfolio/src/Pages/HomePage.jsx
+++ My-Portfolio/src/Pages/HomePage.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import './HomePage.css' 
 
 const HomePage = () => {
   return (
-    <div>This is HomePage</div>
+    <>
+      <div className="social-icons">
+      <a href="#"><i className="fab fa-linkedin"></i></a>
+      <a href="#"><i className="fab fa-twitter"></i></a>
+      <a href="#"><i className="fab fa-instagram"></i></a>
+      <a href="#"><i className="fas fa-envelope"></i></a>
+      <a href="#"><i className="fab fa-github"></i></a>
+    </div>
+      <section className="hero">
+      <img src="/logo.svg" alt="Logo" className="logo" />
+
+      <div className="hero-text">
+        <h1>Anurag Singh</h1>
+        <p className="subtitle">MERN Stack Developer</p>
+
+        <div className="hero-buttons">
+          <button className="btn about">About Me →</button>
+          <button className="btn works">Latest Works</button>
+        </div>
+      </div>
+
+      <div className="chat-bubble">
+        <span>Chat with us</span>
+        <img src={profile} alt="Profile" className="chat-avatar" />
+      </div>
+    </section>
+    </>
   )
 }
 
